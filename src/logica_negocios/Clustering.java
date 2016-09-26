f2bc7aa06be1530e108b25165056f07178ac325f
precagno <pablosrecagno88@gmail.com>
2016-09-26 16:14:43 -0300

    Funcionalidad de ordenar lista de aristas de mayor a menor en la clase Clustering: terminada

--- src/logica_negocios/Clustering.java
+++ src/logica_negocios/Clustering.java
@@ -1,27 +1,50 @@
 package logica_negocios;
 
 import java.util.ArrayList;
-
+import java.util.Collections;
 import logica_negocios.Algoritmos.Arista;
-
 
 public class Clustering {
 	
 	//variable de instancia
-	private static ArrayList<Arista>_pesosAristas;
+	private ArrayList<Arista>_pesosAristas;
 	
 	//constructor
-	public Clustering(ArrayList<Arista> _pesosAristas){
-		this._pesosAristas=new ArrayList<Arista>();
-		//this._pesosAristas=(ArrayList<Arista>) Algoritmos._aristasAGM.clone();
+	public Clustering(ArrayList<Arista> aristas){
+		_pesosAristas=clonaAristas(aristas);
 	}
 	
-	//////// pablito en este punto deberiamos traer el ArrayList de aristas ordenado de mayor a menor
-	//////// no me da bola el .clone que intente mepa
-
+	public void ordenaAristasDescendente(){//ordena la lista de aristas de mayor a menor
+		Collections.sort(this._pesosAristas);
+	}
+	
+	public ArrayList<Arista> getPesosAristas(){//devuelve una lista de aristas
+		return _pesosAristas;
+	}
+	
+	/*-- Metodos auxiliares --*/
+	private ArrayList<Arista> clonaAristas(ArrayList<Arista> aristas){//clona un ArrayList de aristas
+		ArrayList<Arista> aux=new ArrayList<Arista>();
+		for(int i=0;i<aristas.size();i++){
+			Arista actual=aristas.get(i);
+			aux.add(new Arista(actual.getVertAGM(),actual.getVertice(),actual.getPeso()));
+		}
+		return aux;
+	}
 	
 	public static void main(String[]args){
-	System.out.println(_pesosAristas.toString());
-	
+		GrafoPesado grafo = new GrafoPesado(5);
+		grafo.agregarArista(0, 1, 5);
+		grafo.agregarArista(0, 2, 6);
+		grafo.agregarArista(0, 3, 10);
+		grafo.agregarArista(1, 2, 1);
+		grafo.agregarArista(2, 3, 5);
+		grafo.agregarArista(1, 4, 4);
+		grafo.agregarArista(2, 4, 10);
+		grafo.agregarArista(3, 4, 15);
+		
+		Clustering clustering=new Clustering(Algoritmos.AGM(grafo).getAristasAGM());
+		clustering.ordenaAristasDescendente();
+		System.out.println(clustering.getPesosAristas().toString());
 	}
 }
