d0daa1e8918e15003b6eeeaddb71fc5d74ba4930
Pablo <pablosrecagno88@gmail.com>
2016-10-15 12:05:27 -0300

    Refactorización clase Clustering

--- src/logica_negocios/Clustering.java
+++ src/logica_negocios/Clustering.java
@@ -13,48 +13,51 @@ public class Clustering {
 	private ArrayList<Arista>_pesosAristas;
 	
 	// Constructor
-	public Clustering(ArrayList<Arista> aristas){
-		_pesosAristas=clonaAristas(aristas);
+	public Clustering(ArrayList<Arista> aristas)
+	{
+		this._pesosAristas=clonaAristas(aristas);
 	}
 	
 	// Obvía las aristas mayores de acuerdo a la cant de clusters -1
-	public ArrayList<Arista> obviarAristasMayores(ArrayList<Arista> aristas, int cantClusters){
+	public ArrayList<Arista> obviarAristasMayores(ArrayList<Arista> aristas, int cantClusters)
+	{
 		ArrayList<Arista> aux=new ArrayList<Arista>();
 		int aristasEliminadas=cantClusters-1;
 		recorreAgrega(aristas, aux, aristasEliminadas);//de, a, indice de inicio
 		return aux;
 	}
 	
-	// Ordena el conj de aristas de > a < peso
-	public void ordenaAristasDescendente(ArrayList<Arista> aristas){
-		Collections.sort(aristas);//para poder utilizar Collections.sort se modificio la clase inner Aristas con compareTo
-		Collections.reverse(aristas);
+	// Lectura protegida de la lista de aristas
+	public ArrayList<Arista> getPesosAristas()
+	{
+		return this._pesosAristas;
 	}
 		
 	/*-- Metodos auxiliares --*/
 	// Clona un ArrayList de aristas
-	private ArrayList<Arista> clonaAristas(ArrayList<Arista> aristas){
+	private ArrayList<Arista> clonaAristas(ArrayList<Arista> aristas)
+	{
 		ArrayList<Arista> aux=new ArrayList<Arista>();
 		recorreAgrega(aristas, aux, 0);//de, a, indice de inicio
 		return aux;
 	}
 
-	// Unifica rrecorrido y agregado de un ArrayList a otro
+	// Unifica recorrido y agregado de un ArrayList a otro
 	private void recorreAgrega(ArrayList<Arista> aristas, ArrayList<Arista> aux, int indice) {
-		for(int i=indice;i<aristas.size();i++){
+		for(int i=indice;i < aristas.size();i++)
+		{
 			Arista actual=aristas.get(i);
 			aux.add(new Arista(actual.getVertAGM(),actual.getVertice(),actual.getPeso()));
 			ordenaAristasDescendente(aux);
 		}
 	}
 	
-	// Lectura protegida de la lista de aristas
-	public ArrayList<Arista> getPesosAristas(){
-		return _pesosAristas;
+	// Ordena el conj de aristas de > a < peso
+	private void ordenaAristasDescendente(ArrayList<Arista> aristas)
+	{
+		Collections.sort(aristas);//para poder utilizar Collections.sort se modificio la clase inner Aristas con compareTo
+		Collections.reverse(aristas);
 	}
-	
-	
-	
 	
 	////////////main a efectos de verificar funcionamiento, luego borrar
 	public static void main(String[]args) throws IOException{
