16f6c92367fc6c92ba82ae2704f3abaed98f29d4
precagno <pablosrecagno88@gmail.com>
2016-10-17 14:32:37 -0300

    Nuevamente los vertices tienen id asignado correctamente

--- src/modelo/DAOVertices.java
+++ src/modelo/DAOVertices.java
@@ -9,18 +9,19 @@ import java.io.IOException;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
 
-public class DAOVertices {
+public class DAOVertices 
+{
 	//variables de instancia
 	private Gson _gson;
 	private String nombreArchivo;
 	private ArrayList<Vertice> listaCoordenadas;
 	
-	// Cosntructor
+	// cosntructor
 	public DAOVertices(String nombreArchivo)throws IOException
 	{
 		this._gson=new Gson();
 		this.nombreArchivo=nombreArchivo;
-		this.listaCoordenadas=this.desserializaJson(this.nombreArchivo);//este tiene que ir
+		this.listaCoordenadas=this.desserializaJson(this.nombreArchivo);
 	}
 
 	// Retorna lista de coordenadas
@@ -33,16 +34,25 @@ public class DAOVertices {
 	private ArrayList<Vertice> desserializaJson(String nombreArchivo)throws IOException
 	{
 		BufferedReader br = new BufferedReader(new FileReader(nombreArchivo));
-		
 		Type collectionType=new TypeToken<ArrayList<Vertice>>(){}.getType();
-		
 		ArrayList<Vertice> listaCoordenadas=_gson.fromJson(br,collectionType);
-		
 		if(listaCoordenadas==null)//s� el archivo est� vacio
 		{
-			listaCoordenadas=new ArrayList<Vertice>();//hago una lista vac�a
+			listaCoordenadas=new ArrayList<Vertice>();//hara una lista vac�a
 		}
 		
+		this.asignaIds(listaCoordenadas);
+		
 		return listaCoordenadas;
+	}
+
+	//Ya que el mapeo objeto json->objeto Vertice no asigna id,lo
+	//hago mediante un for
+	private void asignaIds(ArrayList<Vertice> listaCoordenadas)
+	{
+		for(int i=0;i < listaCoordenadas.size();i++)
+		{
+			listaCoordenadas.get(i).setId(i);
+		}	
 	}
 }
