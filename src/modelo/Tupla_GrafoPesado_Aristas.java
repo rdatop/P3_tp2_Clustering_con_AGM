5fa662c2905ec02ac72f8dd88f5de6e43f7922f1
Pablo <pablosrecagno88@gmail.com>
2016-10-18 02:07:16 -0300

    Clase Tupla_Grafo_pesado_Aristas:emprolijada

--- src/modelo/Tupla_GrafoPesado_Aristas.java
+++ src/modelo/Tupla_GrafoPesado_Aristas.java
@@ -4,45 +4,53 @@ import java.util.ArrayList;
 import logica_negocios.Algoritmos.Arista;
 import logica_negocios.GrafoPesado;
 
-public class Tupla_GrafoPesado_Aristas{
+public class Tupla_GrafoPesado_Aristas
+{
 	//variables de instancia
 	private GrafoPesado _grafoPesado;
 	private ArrayList<Arista> _aristasAGM;
 	
-	// Cosntrutor
-	public Tupla_GrafoPesado_Aristas(ArrayList<Vertice> vertices){
-		_grafoPesado=new GrafoPesado(vertices);
-		_aristasAGM=new ArrayList<Arista>();
+	//Constructor
+	public Tupla_GrafoPesado_Aristas(ArrayList<Vertice> vertices)
+	{
+		this._grafoPesado=new GrafoPesado(vertices);
+		this._aristasAGM=new ArrayList<Arista>();
 	}
 
 	// Agrega aristas del grafo pesado
-	public void agregaAristaGrafoPesado(Vertice aristaAgm,Vertice arista){//int
-		_grafoPesado.agregarArista(aristaAgm,arista);
+	public void agregaAristaGrafoPesado(Vertice aristaAgm,Vertice arista)
+	{
+		this._grafoPesado.agregarArista(aristaAgm,arista);
 	}
 	
 	// Construye una lista con los vertices y aristas del AGM
-	public void agregaAristaAListaAgm(Arista arista){
-		_aristasAGM.add(arista);
+	public void agregaAristaAListaAgm(Arista arista)
+	{
+		this._aristasAGM.add(arista);
 	}
 	
 	// Lectura protegida de grafo Pesado
-	public GrafoPesado getGrafoPesado(){
-		return _grafoPesado;
+	public GrafoPesado getGrafoPesado()
+	{
+		return this._grafoPesado;
 	}
 
 	// Lectura protegida de la lista de vertices y aristas del AGM
-	public ArrayList<Arista> getAristasAGM(){
-		return _aristasAGM;
+	public ArrayList<Arista> getAristasAGM()
+	{
+		return this._aristasAGM;
 	}
 	
 	//Expuesto: contine aristas sin herencia de la class GrafoPesado/Grafo
-	public boolean getContieneArista(int vert_i, int vert_j) {
-		return _grafoPesado.contieneArista(vert_i, vert_j);
+	public boolean getContieneArista(int vert_i, int vert_j)
+	{
+		return this._grafoPesado.contieneArista(vert_i, vert_j);
 	}
 	
     //Expuesto: cant de aristas sin herencia de la class GrafoPesado/Grafo
-	public int getCantAristas() {
-		return _grafoPesado.cantAristas();
+	public int getCantAristas()
+	{
+		return this._grafoPesado.cantAristas();
 	}
 	
 }
