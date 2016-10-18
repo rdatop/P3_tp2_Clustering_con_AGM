b908ca50232c8c2c08e02d4a0034a75b6e6a8645
Pablo <pablosrecagno88@gmail.com>
2016-10-18 01:31:34 -0300

    Clase Algoritmos:emprolijada

--- src/logica_negocios/Algoritmos.java
+++ src/logica_negocios/Algoritmos.java
@@ -1,17 +1,13 @@
 package logica_negocios;
 
-import java.io.IOException;
-import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 
-import modelo.DAOVertices;
 import modelo.Tupla_GrafoPesado_Aristas;
 import modelo.Vertice;
 
-public class Algoritmos{
-	//sin constructor
-	
+public class Algoritmos
+{	
 	// Algoritmo de Prim
 	public static Tupla_GrafoPesado_Aristas AGM(GrafoPesado grafo){
 		//Tupla: grafoPesado + lista de aristas del AGM (dirigido a Clustering)
@@ -19,9 +15,6 @@ public class Algoritmos{
 		Set<Integer>vertAMG=new HashSet<Integer>();//vertices del AGM
 		vertAMG.add(0);//se inicializa
 		
-		//int cantAristas=grafo.cantAristas();
-		
-		//if(cantAristas >0){
 			for(int i=0;i<grafo.cantVertices()-1;i++){//recorro los vertices del grafo hasta el ultimo
 				Arista arista=menorArista(grafo, vertAMG);//crea una arista que sera la de menor valor
 				tupla.agregaAristaGrafoPesado(arista.vertAGM, arista.vertice);//se agrega al AGM 
@@ -30,22 +23,22 @@ public class Algoritmos{
 				vertAMG.add(arista.vertice.getId());//se agrega AGM los vertices por prioridad de peso
 				tupla.agregaAristaAListaAgm(arista);
 			}
-		//}
 		return tupla;
 	}
 		
 	/** Ineer class (clase interna, solo se usa en grafo)*/
 	public static class Arista implements Comparable<Arista>{//adhiere a Comparable
 		//variables de instancia
-		public Vertice vertAGM;//int
-		public Vertice vertice;//int
+		public Vertice vertAGM;
+		public Vertice vertice;
 		public double peso;
 		
 		//constructor
-		public Arista(Vertice verticeIncluido, Vertice verticeNoIncluido, double pesoArista){//int
-			vertAGM=verticeIncluido;
-			vertice=verticeNoIncluido;
-			peso=pesoArista;
+		public Arista(Vertice verticeIncluido, Vertice verticeNoIncluido, double pesoArista)
+		{
+			this.vertAGM=verticeIncluido;
+			this.vertice=verticeNoIncluido;
+			this.peso=pesoArista;
 		}
 		
 		// Equals de object sobreescrito (comparacion por igualdad)
@@ -64,33 +57,37 @@ public class Algoritmos{
 		// CompareTo sobreescrito (comparacion por peso de aristas - Clustering)
 		@Override
 		public int compareTo(Arista otraArista) {
-			if (getPeso()< otraArista.getPeso()) {
+			if (this.getPeso()< otraArista.getPeso()) {
 				return -1;
 			}
-			if (getPeso()> otraArista.getPeso()){
+			if (this.getPeso()> otraArista.getPeso()){
 				return 1;
 			}
 			return 0;
 		}
 		
 		// Lectura protegida
-		public Vertice getVertAGM() {//int
-			return vertAGM;
+		public Vertice getVertAGM()
+		{
+			return this.vertAGM;
 		}
 		
-		public Vertice getVertice() {//int
-			return vertice;
+		public Vertice getVertice()
+		{
+			return this.vertice;
 		}
 		
-		public double getPeso() {
-			return peso;
+		public double getPeso()
+		{
+			return this.peso;
 		}
 		
-		///////////////representacion luego borrar
 		@Override
-		public String toString(){
-			return "["+getVertAGM().getId()+"/"+getVertice().getId()+", "+getPeso()+"]";
+		public String toString()
+		{
+			return "["+this.getVertAGM().getId()+"/"+this.getVertice().getId()+", "+this.getPeso()+"]";
 		}
+		
 	}/**fin de clase inner*/
 	
 	// Retorna la arista de menor peso entre un verticeAGM y uno no AMG
@@ -109,33 +106,4 @@ public class Algoritmos{
 		}
 		return ret;
 	}
-	
-	//////////////////prueba de empleo luego borrar
-	public static void main(String[]args) throws IOException{
-		DAOVertices daoVertices=new DAOVertices("src/modelo/instancia1.json");
-		GrafoPesado grafo = new GrafoPesado(daoVertices.obtenerVertices());
-		
-		for (int i = 0; i <grafo.cantVertices()-1; i++) {
-			grafo.obtenerVertice(i).setId(i);
-			for (int j = i+1; j < grafo.cantVertices(); j++) {
-				grafo.obtenerVertice(j).setId(j);
-				grafo.agregarArista(grafo.obtenerVertice(i),grafo.obtenerVertice(j));
-			}
-		}
-
-		System.out.println("Cantidad de vertices del grafo pesado: "+grafo.cantVertices());
-
-		Tupla_GrafoPesado_Aristas tupla=Algoritmos.AGM(grafo);
-		System.out.println("asi se crea el AGM");
-		System.out.println(tupla.getAristasAGM().toString());
-		System.out.println("asi se ordena de > a <");
-		Collections.sort(tupla.getAristasAGM());
-		Collections.reverse(tupla.getAristasAGM());
-		System.out.println(tupla.getAristasAGM().toString());
-		System.out.println("asi queda definitivamente");
-		System.out.println(tupla.getAristasAGM().toString());
-
-}
-
-
 }
