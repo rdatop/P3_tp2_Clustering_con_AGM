0d05470c30f41cdb763241c388ebe23bc5e569ef
Pablo <pablosrecagno88@gmail.com>
2016-10-18 01:44:09 -0300

    Clase Grafo:emprolijada

--- src/logica_negocios/Grafo.java
+++ src/logica_negocios/Grafo.java
@@ -6,95 +6,113 @@ import java.util.Set;
 
 import modelo.Vertice;
 
-public class Grafo {
+public class Grafo
+{
 	//variables de instancia
 	private ArrayList<Vertice> _vertices;
 	private ArrayList<HashSet<Integer>> _vecinos;
 	private int _aristas;//se iran sumando a medida que se agregen
 	
 	//constructor
-	public Grafo(ArrayList<Vertice> listaVertices){
-		_vertices=listaVertices;
-		_vecinos=new ArrayList<HashSet<Integer>>();
-		for (int i = 0; i < cantVertices(); i++) {
-			_vecinos.add(new HashSet<Integer>());
+	public Grafo(ArrayList<Vertice> listaVertices)
+	{
+		this._vertices=listaVertices;
+		this._vecinos=new ArrayList<HashSet<Integer>>();
+		for (int i = 0; i < this.cantVertices(); i++)
+		{
+			this._vecinos.add(new HashSet<Integer>());
 		}
 	}
 	
 	// Entrega la lista de vertices
-	public ArrayList<Vertice> obtenerVertices(){
-		return _vertices;
+	public ArrayList<Vertice> obtenerVertices()
+	{
+		return this._vertices;
 	}
 	
 	// Entrega un vertice particular
-	public Vertice obtenerVertice(int idVertice){
-		return _vertices.get(idVertice);
+	public Vertice obtenerVertice(int idVertice)
+	{
+		return this._vertices.get(idVertice);
 	}
 	
 	// Cant de vertices de grafo
-	public int cantVertices(){
-		return _vertices.size();
+	public int cantVertices()
+	{
+		return this._vertices.size();
 	}
 	
 	// Cant de Vecinos
-	public int cantVecinos(){
-		return _vecinos.size();
+	public int cantVecinos()
+	{
+		return this._vecinos.size();
 	}
 	
 	// Cant de Aristas
-	public int cantAristas(){
-		return _aristas;
+	public int cantAristas()
+	{
+		return this._aristas;
 	}
 	
 	// Relaciona dos vertices mediante una aristas
-	public void agregarAristas(Vertice idVert_i, Vertice idVert_j){//int
-		chequearExtremos(idVert_i.getId(), idVert_j.getId());//verifica rangos
-		if(!contieneArista(idVert_i.getId(),idVert_j.getId())){//verifica que no esten asignados
-			_aristas++;//incremente cant aristas
+	public void agregarAristas(Vertice idVert_i, Vertice idVert_j)
+	{
+		this.chequearExtremos(idVert_i.getId(), idVert_j.getId());//verifica rangos
+		if(!this.contieneArista(idVert_i.getId(),idVert_j.getId()))//verifica que no esten asignados
+		{
+			this._aristas++;//incremente cant aristas
 		}
-		_vecinos.get(idVert_i.getId()).add(idVert_j.getId());//
-		_vecinos.get(idVert_j.getId()).add(idVert_i.getId());//asignacion simetrica
+		this._vecinos.get(idVert_i.getId()).add(idVert_j.getId());//
+		this._vecinos.get(idVert_j.getId()).add(idVert_i.getId());//asignacion simetrica
 	}
 	
 	// Desvincula dos vertices eliminando la arista q los unio
-	public void removerArista(int idVert_i, int idVert_j){
-		chequearExtremos(idVert_i, idVert_j);
-		if(contieneArista(idVert_i, idVert_j)){//si esta asignado
-			_aristas --;//decrementa cant aristas 
+	public void removerArista(int idVert_i, int idVert_j)
+	{
+		this.chequearExtremos(idVert_i, idVert_j);
+		if(this.contieneArista(idVert_i, idVert_j)){//si esta asignado
+			this._aristas --;//decrementa cant aristas 
 		}
-		_vecinos.get(idVert_i).remove(idVert_j);//
-		_vecinos.get(idVert_j).remove(idVert_i);//desasignacion asimetrica
+		this._vecinos.get(idVert_i).remove(idVert_j);//
+		this._vecinos.get(idVert_j).remove(idVert_i);//desasignacion asimetrica
 	}
 		
 	// Verifica asignacion entre vertices
-	public boolean contieneArista(int idVert_i, int idVert_j){
-		chequearExtremos(idVert_i,idVert_j);
-		return _vecinos.get(idVert_i).contains(idVert_j);
+	public boolean contieneArista(int idVert_i, int idVert_j)
+	{
+		this.chequearExtremos(idVert_i,idVert_j);
+		return this._vecinos.get(idVert_i).contains(idVert_j);
 	}
 
 	// Verifica limites de los parametros
-	private void chequearExtremos(int idVert_i, int idVert_j) {
-		if (idVert_i <= -1 || idVert_j <= -1 || idVert_i >= cantVecinos() || idVert_j >= cantVecinos()){//verifica rango
+	private void chequearExtremos(int idVert_i, int idVert_j)
+	{
+		if (idVert_i <= -1 || idVert_j <= -1 || idVert_i >= this.cantVecinos() || idVert_j >= this.cantVecinos())//verifica rango
+		{
 			throw new IllegalArgumentException("Vertices fuera de rango: " + idVert_i + ", " + idVert_j + " (vertices = " + cantVecinos() + ")");
 		}
-//		if(idVert_i==idVert_j){//verifica que lo tenga bucles
-//			throw new IllegalArgumentException("No se pueden agregar loops: " + idVert_i);
-//		}
+		if(idVert_i==idVert_j)//verifica que lo tenga bucles
+		{
+			throw new IllegalArgumentException("No se pueden agregar loops: " + idVert_i);
+		}
 	}
 	
 	// Entrega el grado del vertice
-	public int gradoDelVertice(int idVertice){
-		return _vecinos.get(idVertice).size();
+	public int gradoDelVertice(int idVertice)
+	{
+		return this._vecinos.get(idVertice).size();
 	}
 	
 	// Entrega los vecinos del vertice
-	public Set<Integer> vecinosDelVertice(int idVertice){
-		Set<Integer> ret=_vecinos.get(idVertice);
+	public Set<Integer> vecinosDelVertice(int idVertice)
+	{
+		Set<Integer> ret=this._vecinos.get(idVertice);
 		return ret;
 	}
 
 	// Verifica la existencia de un vertice tomando como parámetro su id
-	public boolean contieneVertice(int idVertice){
+	public boolean contieneVertice(int idVertice)
+	{
 		return idVertice >= 0 && idVertice <= cantVertices();
 	}
 }
