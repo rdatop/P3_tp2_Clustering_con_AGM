269f53434b27a8b5d109518a015b4a1cb9090998
Pablo <pablosrecagno88@gmail.com>
2016-10-18 01:34:47 -0300

    Clase Clustering:emprolijada

--- src/logica_negocios/Clustering.java
+++ src/logica_negocios/Clustering.java
@@ -1,14 +1,12 @@
 package logica_negocios;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 
 import logica_negocios.Algoritmos.Arista;
-import modelo.DAOVertices;
 
-public class Clustering {
-	
+public class Clustering
+{
 	//variable de instancia
 	private ArrayList<Arista>_pesosAristas;
 	
@@ -23,7 +21,7 @@ public class Clustering {
 	{
 		ArrayList<Arista> aux=new ArrayList<Arista>();
 		int aristasEliminadas=cantClusters-1;
-		recorreAgrega(aristas, aux, aristasEliminadas);//de, a, indice de inicio
+		this.recorreAgrega(aristas, aux, aristasEliminadas);//de, a, indice de inicio
 		return aux;
 	}
 	
@@ -38,17 +36,18 @@ public class Clustering {
 	private ArrayList<Arista> clonaAristas(ArrayList<Arista> aristas)
 	{
 		ArrayList<Arista> aux=new ArrayList<Arista>();
-		recorreAgrega(aristas, aux, 0);//de, a, indice de inicio
+		this.recorreAgrega(aristas, aux, 0);//de, a, indice de inicio
 		return aux;
 	}
 
 	// Unifica recorrido y agregado de un ArrayList a otro
-	private void recorreAgrega(ArrayList<Arista> aristas, ArrayList<Arista> aux, int indice) {
+	private void recorreAgrega(ArrayList<Arista> aristas, ArrayList<Arista> aux, int indice)
+	{
 		for(int i=indice;i < aristas.size();i++)
 		{
 			Arista actual=aristas.get(i);
 			aux.add(new Arista(actual.getVertAGM(),actual.getVertice(),actual.getPeso()));
-			ordenaAristasDescendente(aux);
+			this.ordenaAristasDescendente(aux);
 		}
 	}
 	
@@ -58,24 +57,4 @@ public class Clustering {
 		Collections.sort(aristas);//para poder utilizar Collections.sort se modificio la clase inner Aristas con compareTo
 		Collections.reverse(aristas);
 	}
-	
-	////////////main a efectos de verificar funcionamiento, luego borrar
-	public static void main(String[]args) throws IOException{
-		DAOVertices daoVertices=new DAOVertices("src/modelo/instancia1.json");
-		GrafoPesado grafo = new GrafoPesado(daoVertices.obtenerVertices());
-		for (int i = 0; i <grafo.cantVertices()-1; i++) {
-			grafo.obtenerVertice(i).setId(i);
-			for (int j = i+1; j < grafo.cantVertices(); j++) {
-				grafo.obtenerVertice(j).setId(j);
-				grafo.agregarArista(grafo.obtenerVertice(i),grafo.obtenerVertice(j));
-			}
-		}
-
-		Clustering clustering=new Clustering(Algoritmos.AGM(grafo).getAristasAGM());
-		System.out.println(" Aristas ya ordenadas de > a <");
-		System.out.println(clustering.getPesosAristas().toString());
-		
-		System.out.println(" Prueba clustering");
-		System.out.println(clustering.obviarAristasMayores(clustering.getPesosAristas(),3).toString());
-	}	
 }
